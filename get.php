401ff91bd1837266a702fb306035d8b31ecef440
OpenShift guest <sonalprasad.sp@gmail.com>
2014-04-05 02:56:35 -0400

    adding get

---
+++ get.php
@@ -0,0 +1,22 @@
+<?php
+
+if(!$_GET)
+{
+    $message['error'] = 'true';
+    $message['message'] = 'Must provide url data';
+    return json_encode($message);
+}
+
+$qry_str = $_GET['tag'];
+$ch = curl_init();
+
+// Set query data here with the URL
+curl_setopt($ch, CURLOPT_URL, 'https://api.vineapp.com/timelines/tags/' . $qry_str); 
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, '3');
+$content = trim(curl_exec($ch));
+curl_close($ch);
+print $content;
+
+?>
---
+++ mainMenu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+    
+        <title>DanceSea Main Menu</title>
+    
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+        <!-- Custom styles for this template -->
+        <link href="cover.css" rel="stylesheet">
+    
+    
+    </head>
